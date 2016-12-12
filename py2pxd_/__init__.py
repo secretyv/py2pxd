d16895c40068069cda61d459f0c3ba6409f3f29a
secretyv <yves.secretan@ete.inrs.ca>
2016-12-12 15:06:59 -0500

    Load all the modules files in a single namespace

---
+++ py2pxd_/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pxreader   import PXReader
+from pxvariable import PXVariable
+from pxfunction import PXFunction
+from pxclass    import PXClass
+from pxmodule   import PXModule, __version__, header
+
+
